d67c598bb049c76acd91a8588c3655c0de15de5a
David Durieux <d.durieux@siprossii.com>
2015-11-14 15:16:38 +0100

    Prepare module nsca to new alignak module system and prepare too to publish on pip

---
+++ alignak_module_nsca/__init__.py
@@ -0,0 +1,6 @@
+
+from .version import VERSION as __version__
+
+# the properties and get_instance must be available from the top-level:
+from .nsca import properties, get_instance
+
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+
+from setuptools import setup, find_packages
+
+import os
+import setuptools
+
+
+# Fix for debian 7 python that raise error on at_exit at the end of setup.py
+# (cf http://bugs.python.org/issue15881)
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+# Better to use exec to load the VERSION so to not have to import the alignak package:
+with open(os.path.join('alignak_module_nsca', 'version.py')) as fh:
+    ns = {}
+    exec(fh.read(), ns)
+    VERSION = ns['VERSION']
+
+
+os.environ['PBR_VERSION'] = VERSION
+
+setuptools.setup(
+    setup_requires=['pbr'],
+    version=VERSION,
+    pbr=True,
+)
